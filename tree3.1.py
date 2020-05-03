611a20159128b863542e2e013a464592d10e168e
CUNGEN TIAN <2018tian@gmail.com>
2020-05-03 14:15:28 +0800

    避免重复实验,提高效率

--- tree3.1.py
+++ tree3.1.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import numpy as np
 import random
-import time
 names = locals()
 leaves = {}
 black_branch = []
@@ -17,68 +16,84 @@ class Branch:
         self.situation = situation
         self.name_subbranch = []  # 子节点ID
         self.num_subbranch = 0  # 子节点总量
+        self.unexpanded_subbranch = []#未完全拓展子节点
+        self.created = 0 #节点是否已创建
         self.leaf = 0  # 是否为叶
         if sum(sum(self.situation)) == 0:
             self.leaf = 1
         self.current_player = 1  # 1和-1表示两个参与者
 
     def creat_subbranch(self):
+        """找到可行的子节点,并将子节点初始化"""
         if self.leaf:
             return 0
+        if self.created:
+            return 1
         self.num_subbranch = 0
         self.name_subbranch = []
-        for len in range(4):  # line
+        number = 0
+        for len in range(4):  # 行方向
             for i in range(4):
                 for j in range(4-len):
                     if sum(self.situation[i, j:j+len+1]) == len+1:
+                        number += 1  # Id序号
+                        sub_name = self.name+'_'+str(number)
+                        if sub_name not in black_branch:
+                            self.num_subbranch += 1
+                            if sub_name not in names.keys():
+                                new_situation = self.situation.copy()
+                                new_situation[i, j:j+len +
+                                              1] = np.zeros([1, len+1])
+                                names[sub_name] = Branch(
+                                    new_situation, sub_name, self.name)
+                                names[sub_name].current_player = self.current_player * \
+                                    (-1)
+                                self.name_subbranch.append(sub_name)
+                                self.unexpanded_subbranch.append(sub_name)
+                                if sum(sum(new_situation)) == 0:  # 遇见叶
+                                    names[sub_name].leaf = 1
+                                    names[sub_name].matchs = 1
+                                    leaves[sub_name] = names[sub_name].current_player
+                                    self.unexpanded_subbranch.remove(sub_name)
+                                    temp_0 = self
+                                    while temp_0.unexpanded_subbranch == []:
+                                        names[temp_0.father].unexpanded_subbranch.remove(
+                                            temp_0.name)
+                                        temp_0 = names[temp_0.father]
 
-                        self.num_subbranch += 1
-                        new_situation = self.situation.copy()
-                        new_situation[i, j:j+len+1] = np.zeros([1, len+1])
-
-                        sub_name = self.name+'_'+str(self.num_subbranch)
-                        # 创建分支
-                        if sub_name not in names and sub_name not in black_branch:
-                            names[sub_name] = Branch(
-                                new_situation, sub_name, self.name)
-                        names[sub_name].current_player = self.current_player * \
-                            (-1)
-                        if sum(sum(new_situation)) == 0:  # 遇见叶
-                            names[sub_name].leaf = 1
-                            names[sub_name].matchs = 1
-                            leaves[sub_name] = names[sub_name].current_player
-                        self.name_subbranch.append(sub_name)
-
-        for len in [1, 2, 3]:  # raw
+        for len in [1, 2, 3]:  # 列方向
             for i in range(4-len):
                 for j in range(4):
                     if sum(self.situation[i:i+len+1, j]) == len+1:
-                        self.num_subbranch += 1
-                        new_situation = self.situation.copy()
-                        new_situation[i:i+len+1, j] = np.zeros(len+1)
-                        sub_name = self.name+'_'+str(self.num_subbranch)
-                        if sub_name not in names and sub_name not in black_branch:
-                            names[sub_name] = Branch(
-                                new_situation, sub_name, self.name)
-                        names[sub_name].current_player = self.current_player * \
-                            (-1)
-                        if sum(sum(new_situation)) == 0:
-                            names[sub_name].leaf = 1
-                            names[sub_name].matchs = 1
-                            leaves[sub_name] = names[sub_name].current_player
-                        self.name_subbranch.append(sub_name)
+                        number += 1  # Id序号
+                        sub_name = self.name+'_'+str(number)
+                        if sub_name not in black_branch:
+                            if sub_name not in names.keys():
+                                self.num_subbranch += 1
+                                new_situation = self.situation.copy()
+                                new_situation[i:i+len+1, j] = np.zeros(len+1)
+                                names[sub_name] = Branch(
+                                    new_situation, sub_name, self.name)
+                                names[sub_name].current_player = self.current_player * \
+                                    (-1)
+                                self.name_subbranch.append(sub_name)
+                                self.unexpanded_subbranch.append(sub_name)
+                                if sum(sum(new_situation)) == 0:  # 遇见叶
+                                    names[sub_name].leaf = 1
+                                    names[sub_name].matchs = 1
+                                    leaves[sub_name] = names[sub_name].current_player
+                                    self.unexpanded_subbranch.remove(sub_name)
+                                    temp_0 = self
+                                    while temp_0.unexpanded_subbranch == []:
+                                        names[temp_0.father].unexpanded_subbranch.remove(
+                                            temp_0.name)
+                                        temp_0 = names[temp_0.father]
+
+        self.created = 1
         return self.num_subbranch-1
 
-    def compare_branch(self, situation):  # 输入局势,与子节点对比,返回子节点名称
-        for name in self.name_subbranch:
-            if (names[name].situation == situation).all():
-                print("=====")
-                print(names[name].situation)
-                print(situation)
-                return name
-        return False
-
     def sata1(self):
+        """统计试验结果"""
         temp = [0, 0]
         if self.num_subbranch == 0:
             return [0, 1]
@@ -92,17 +107,20 @@ class Branch:
         return temp
 
     def expend1(self):
-        if self.creat_subbranch():
-            temp0 = self
-            while(temp0.creat_subbranch()):
-                while 1:
-                    temp1 = temp0.name_subbranch[random.randint(
-                        1, temp0.num_subbranch)-1]
-                    if(names[temp1].leaf == 0):
-                        break
-                temp0 = names[temp1]
+        """进行一次试验"""
+        temp0 = self
+        while(temp0.creat_subbranch()):
+            if(temp0.unexpanded_subbranch == []):
+                if temp0 == self: # self已被完全拓展
+                    return 2
+                return 0
+            temp1 = temp0.unexpanded_subbranch[random.randint(
+                1, len(temp0.unexpanded_subbranch))-1]
+            temp0 = names[temp1]
+        return 1
 
     def play_input(self):
+        """用户输入"""
         ary = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [
                        9, 10, 11, 12], [13, 14, 15, 16]], dtype=int)
         while 1:
@@ -137,6 +155,7 @@ class Branch:
             print('Invalid  input !!!')
 
     def del_black(self):
+        """剔除劣势节点"""
         change = 1
         while change:
             if self.name_subbranch == []:
@@ -161,6 +180,7 @@ class Branch:
                         leaves[name_pa] = player
 
 
+
 print('\n'+"NEW GAME".center(54, '-'))
 C = 0.001
 a = np.ones([4, 4], dtype=np.int8)
@@ -172,9 +192,16 @@ current_branch = names[name]
 
 while 1:
     current_branch.creat_subbranch()
+
+    times = 1200
+    if current_branch.num_subbranch <= 27:
+        times = 2400
+        print("!!!!!!!!!!!!!")
     for name in current_branch.name_subbranch:
-        for i in range(1000//current_branch.num_subbranch):
-            names[name].expend1()
+        for i in range(times//current_branch.num_subbranch):
+
+            if (names[name].expend1()==2):
+                break
     current_branch.del_black()
     if current_branch.num_subbranch == 0:
         print('----------------------------')
